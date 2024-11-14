d62ad9728d907109655106eeb9c9a834b78b9944
olegraikevich <oraikevich@sardinasystems.com>
2024-11-14 23:51:43 +0400

    update CreateTodoApiTest

--- src/test/kotlin/CreateTodoApiTest.kt
+++ src/test/kotlin/CreateTodoApiTest.kt
@@ -3,35 +3,72 @@ package todo.api
 import io.restassured.RestAssured.given
 import io.restassured.http.ContentType
 import org.junit.jupiter.api.Test
+import org.hamcrest.Matchers.containsString
 
 
 class CreateTodoApiTest: BaseApiTest() {
 
-    val randomId = (1..100).random()
-
     @Test
-    fun `test create todo`() {
+    fun `positive test create todo`() {
         val requestBody = """{
             "id": ${TestData.staticId},
             "text": "do something",
             "completed": false
         }"""
 
-        // check creation status
         given()
             .contentType(ContentType.JSON)
             .body(requestBody)
             .post("/todos")
             .then()
                 .statusCode(201)
+    }
 
-        // negative check duplicate creation
+    @Test
+    fun `duplicate id create todo`() {
+        val requestBody = """{
+            "id": ${TestData.staticId},
+            "text": "do something",
+            "completed": false
+        }"""
+
         given()
             .contentType(ContentType.JSON)
             .body(requestBody)
             .post("/todos")
             .then()
-            .statusCode(409)
+            .statusCode(400)
     }
 
+    @Test
+    fun `incorrect id create todo`() {
+        val randomNumber = (1..100).random()
+
+        val requestBody = """{
+            "id": ${TestData.staticId},
+            "text": $randomNumber,
+            "completed": false
+        }"""
+
+        given()
+            .contentType(ContentType.JSON)
+            .body(requestBody)
+            .post("/todos")
+            .then()
+            .statusCode(400)
+            .body(containsString("Request body deserialize error: invalid type: integer"))
+    }
+
+    @Test
+    fun `empty body create todo`() {
+        val requestBody = """"""
+
+        given()
+            .contentType(ContentType.JSON)
+            .body(requestBody)
+            .post("/todos")
+            .then()
+            .statusCode(400)
+            .body(containsString("Request body deserialize error: EOF while parsing"))
+    }
 }
