a4b62158a8c5cf0b79dcb553fc0d743adba845cb
olegraikevich <oraikevich@sardinasystems.com>
2024-11-16 14:37:09 +0400

    update test data

--- src/test/kotlin/TestData.kt
+++ src/test/kotlin/TestData.kt
@@ -1,18 +1,52 @@
 package todo.api
 
+import io.restassured.RestAssured.given
+import io.restassured.http.ContentType
+
 object TestData {
-    val staticId: Int = (100..999).random()
-    val randomString: String = (1..200)
+
+    val uniqId: Int = (100..999).random()
+    const val NONEXISTENTID = 0
+
+    val randomString: String = (1..20)
         .map { ('a'..'z').random() }
         .joinToString("")
 
-    fun generateRandomId(): Int {
+    private fun generateRandomId(): Int {
         var randomUniqId: Int
         do {
             randomUniqId = (100..999).random()
-        } while (randomUniqId == staticId)
+        } while (randomUniqId == uniqId)
 
         return randomUniqId
     }
 
+    fun generateUniqEntity() {
+        val requestBody = """{
+            "id": $uniqId,
+            "text": "$randomString",
+            "completed": false
+            }"""
+
+        given()
+            .contentType(ContentType.JSON)
+            .body(requestBody)
+            .post("/todos")
+    }
+
+    fun generateEntities(todoQuantity: Int) {
+
+        repeat(todoQuantity) {
+            val requestBody = """{
+            "id": ${generateRandomId()},
+            "text": "$randomString",
+            "completed": false
+            }"""
+
+            given()
+                .contentType(ContentType.JSON)
+                .body(requestBody)
+                .post("/todos")
+        }
+    }
 }
