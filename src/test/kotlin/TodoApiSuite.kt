9a6d6f4414ca7278a1c8a2d8dd4d999f807903ea
olegraikevich <oraikevich@sardinasystems.com>
2024-11-16 14:35:13 +0400

    add suite for api tests

---
+++ src/test/kotlin/TodoApiSuite.kt
@@ -0,0 +1,17 @@
+package todo.api
+
+import org.junit.jupiter.api.ClassOrderer.OrderAnnotation
+import org.junit.jupiter.api.TestClassOrder
+import org.junit.platform.suite.api.SelectClasses
+import org.junit.platform.suite.api.Suite
+
+@Suite
+@SelectClasses(
+    CreateTodoApiTest::class,
+    GetListTodoApiTest::class,
+    UpdateTodoApiTest::class,
+    DeleteTodoApiTest::class
+)
+@TestClassOrder(OrderAnnotation::class)
+class TodoApiSuite {
+}
