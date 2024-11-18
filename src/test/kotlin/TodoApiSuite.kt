80e3fe78998a159d207a0701407dadbd2fcf797d
olegraikevich <oraikevich@sardinasystems.com>
2024-11-18 05:07:55 +0400

    fix imports

--- src/test/kotlin/TodoApiSuite.kt
+++ src/test/kotlin/TodoApiSuite.kt
@@ -1,7 +1,5 @@
 package todo.api
 
-import org.junit.jupiter.api.ClassOrderer.OrderAnnotation
-import org.junit.jupiter.api.TestClassOrder
 import org.junit.platform.suite.api.SelectClasses
 import org.junit.platform.suite.api.Suite
 
